16a6475e5d17c7b206625c0e1196a22803134270
kboygit <heybisayangdako@gmail.com>
2016-12-19 12:23:56 -0800

    create a new function drawBall,invoked the function

--- script.js
+++ script.js
@@ -8,14 +8,18 @@ var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
-function draw(){
-  ctx.clearRect(0,0,canvas.width, canvas.height);
-  // Drawing Code
+function drawBall(){
   ctx.beginPath();
   ctx.arc(x , y, 10, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+}
+
+function draw(){
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  // Drawing Code
+  drawBall();
   x += dx;
   y += dy;
 }
